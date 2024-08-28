fa08edaef4e0cac863404d02c235e913f206f917
kelvin wepo <kelvinwepo7710@gmail.com>
2024-08-28 15:15:02 +0300

    modification

--- home/views.py
+++ home/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.models import User  # Importing User model
 from .models import UserProfile, Certification, Language, RatingSeller  # Importing models from current directory
 from django.forms import inlineformset_factory  # Importing inlineformset_factory for formset creation
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 
 
 # View function for the introductory home page
@@ -49,103 +50,228 @@ def home(request, identifier):
     return render(request, 'home.html', context)
 
 # View function for editing user profile
-@csrf_exempt
-@login_required()  # Requiring login for access
+# @csrf_exempt
+# @login_required()  # Requiring login for access
+# def edit_profile(request, identifier):
+#     user = get_object_or_404(User, username=identifier)  # Getting the user object
+#     user_profile, created = UserProfile.objects.get_or_create(user=user)
+
+#     # Initialize context with common values
+#     current_user = request.user
+#     if identifier == current_user.username:
+#         pass
+#     else:
+#         return HttpResponseForbidden("Access Denied")
+
+#     context = {
+#         'user': user,
+#         'user_profile': user_profile,
+#     }
+
+#     # Checking authorization for profile editing
+#     if request.user != user:
+#         raise PermissionDenied("You are not authorized to edit this profile.")
+
+#     # Creating form instances for user profile, certifications, and languages
+#     user_form = UserProfileForm(request.POST or None, request.FILES or None, instance=user_profile)
+#     CertificationFormSet = inlineformset_factory(UserProfile, Certification, form=CertificationForm, extra=1)
+#     certification_queryset = Certification.objects.filter(user_profile=user_profile)
+#     certification_formset = CertificationFormSet(request.POST or None, instance=user_profile, prefix='certifications', queryset=certification_queryset)
+#     LanguageFormSet = inlineformset_factory(UserProfile, Language, form=LanguageForm, extra=1)
+#     language_queryset = Language.objects.filter(user_profile=user_profile)
+#     language_formset = LanguageFormSet(request.POST or None, instance=user_profile, prefix='languages', queryset=language_queryset)
+
+#     # Handling form submission
+#     username_taken = False
+#     email_taken = False
+#     if request.method == 'POST':
+#         new_username = request.POST['username']
+#         new_email = request.POST['email']
+
+#         # Checking if new username or email is already taken
+#         username_taken = User.objects.filter(username=new_username).exclude(pk=user.pk).exists()
+#         email_taken = User.objects.filter(email=new_email).exclude(pk=user.pk).exists()
+
+#         if username_taken or email_taken:
+#             if username_taken:
+#                 context['username_message'] = "Username is already taken. Please choose another."
+#             if email_taken:
+#                 context['email_message'] = "Email is already taken. Please choose another."
+#         else:
+#             # Saving user details if form data is valid
+#             user.username = new_username
+#             user.email = new_email
+#             user.first_name = request.POST['first_name']
+#             user.last_name = request.POST['last_name']
+#             user.save()
+
+#             if user_form.is_valid() and certification_formset.is_valid() and language_formset.is_valid():
+#                 user_form.save()
+
+#                 # Deleting marked certifications
+#                 for form in certification_formset.deleted_forms:
+#                     if form.cleaned_data.get('id'):
+#                         form.instance.delete()
+
+#                 # Deleting marked languages
+#                 for form in language_formset.deleted_forms:
+#                     if form.cleaned_data.get('id'):
+#                         form.instance.delete()
+
+#                 # Saving valid certifications
+#                 for form in certification_formset:
+#                     if form.is_valid() and not form.cleaned_data.get('id'):
+#                         certification = form.save(commit=False)
+#                         if certification.title and certification.issuing_organization:
+#                             certification.user_profile = user_profile
+#                             certification.save()
+
+#                 # Saving valid languages
+#                 for form in language_formset:
+#                     if form.is_valid() and not form.cleaned_data.get('id'):
+#                         language = form.save(commit=False)
+#                         if language.language and language.proficiency:
+#                             language.user_profile = user_profile
+#                             language.save()
+
+#                 # Redirecting to the user's introductory home page
+#                 return redirect('IntroHome')
+
+#     # Updating context with form instances and other necessary data
+#     context.update({
+#         'user_form': user_form,
+#         'certification_formset': certification_formset,
+#         'language_formset': language_formset,
+#         'username_taken': username_taken,
+#         'email_taken': email_taken,
+#     })
+#     return render(request, 'edit_profile.html', context)
+
+
+@login_required
 def edit_profile(request, identifier):
-    user = get_object_or_404(User, username=identifier)  # Getting the user object
+    user = get_object_or_404(User, username=identifier)
     user_profile, created = UserProfile.objects.get_or_create(user=user)
 
-    # Initialize context with common values
-    current_user = request.user
-    if identifier == current_user.username:
-        pass
-    else:
+    if request.user != user:
         return HttpResponseForbidden("Access Denied")
 
+    user_form = UserProfileForm(request.POST or None, request.FILES or None, instance=user_profile)
+    CertificationFormSet = inlineformset_factory(UserProfile, Certification, form=CertificationForm, extra=1)
+    certification_formset = CertificationFormSet(request.POST or None, request.FILES or None, instance=user_profile, prefix='certifications')
+    LanguageFormSet = inlineformset_factory(UserProfile, Language, form=LanguageForm, extra=1)
+    language_formset = LanguageFormSet(request.POST or None, request.FILES or None, instance=user_profile, prefix='languages')
+
+    if request.method == 'POST':
+        new_username = request.POST.get('username')
+        new_email = request.POST.get('email')
+
+        username_taken = User.objects.filter(username=new_username).exclude(pk=user.pk).exists()
+        email_taken = User.objects.filter(email=new_email).exclude(pk=user.pk).exists()
+
+        if username_taken:
+            messages.error(request, "Username is already taken.")
+        elif email_taken:
+            messages.error(request, "Email is already taken.")
+        elif user_form.is_valid() and certification_formset.is_valid() and language_formset.is_valid():
+            user.username = new_username
+            user.email = new_email
+            user.first_name = request.POST.get('first_name')
+            user.last_name = request.POST.get('last_name')
+            user.save()
+
+            user_profile = user_form.save(commit=False)
+            user_profile.user = user
+
+            if request.FILES.get('profile_image'):
+                user_profile.profile_image = request.FILES['profile_image']
+            
+            user_profile.save()
+
+            certification_formset.instance = user_profile
+            certification_formset.save()
+            language_formset.instance = user_profile
+            language_formset.save()
+
+            messages.success(request, "Profile updated successfully.")
+            return redirect('home', identifier=user.username)
+        else:
+            messages.error(request, "Please correct the errors below.")
+
     context = {
+        'user_form': user_form,
+        'certification_formset': certification_formset,
+        'language_formset': language_formset,
         'user': user,
         'user_profile': user_profile,
     }
 
-    # Checking authorization for profile editing
-    if request.user != user:
-        raise PermissionDenied("You are not authorized to edit this profile.")
+    return render(request, 'edit_profile.html', context)
 
-    # Creating form instances for user profile, certifications, and languages
+    user = get_object_or_404(User, username=identifier)
+    user_profile, created = UserProfile.objects.get_or_create(user=user)
+
+    # Check if the current user is the profile owner
+    if request.user != user:
+        return HttpResponseForbidden("Access Denied")
+
+    # Creating form instances
     user_form = UserProfileForm(request.POST or None, request.FILES or None, instance=user_profile)
     CertificationFormSet = inlineformset_factory(UserProfile, Certification, form=CertificationForm, extra=1)
-    certification_queryset = Certification.objects.filter(user_profile=user_profile)
-    certification_formset = CertificationFormSet(request.POST or None, instance=user_profile, prefix='certifications', queryset=certification_queryset)
+    certification_formset = CertificationFormSet(request.POST or None, request.FILES or None, instance=user_profile, prefix='certifications')
     LanguageFormSet = inlineformset_factory(UserProfile, Language, form=LanguageForm, extra=1)
-    language_queryset = Language.objects.filter(user_profile=user_profile)
-    language_formset = LanguageFormSet(request.POST or None, instance=user_profile, prefix='languages', queryset=language_queryset)
+    language_formset = LanguageFormSet(request.POST or None, request.FILES or None, instance=user_profile, prefix='languages')
 
-    # Handling form submission
-    username_taken = False
-    email_taken = False
     if request.method == 'POST':
-        new_username = request.POST['username']
-        new_email = request.POST['email']
+        new_username = request.POST.get('username')
+        new_email = request.POST.get('email')
 
-        # Checking if new username or email is already taken
+        # Check if new username or email is already taken
         username_taken = User.objects.filter(username=new_username).exclude(pk=user.pk).exists()
         email_taken = User.objects.filter(email=new_email).exclude(pk=user.pk).exists()
 
-        if username_taken or email_taken:
-            if username_taken:
-                context['username_message'] = "Username is already taken. Please choose another."
-            if email_taken:
-                context['email_message'] = "Email is already taken. Please choose another."
-        else:
-            # Saving user details if form data is valid
+        if username_taken:
+            messages.error(request, "Username is already taken.")
+        elif email_taken:
+            messages.error(request, "Email is already taken.")
+        elif user_form.is_valid() and certification_formset.is_valid() and language_formset.is_valid():
+            # Update user details
             user.username = new_username
             user.email = new_email
-            user.first_name = request.POST['first_name']
-            user.last_name = request.POST['last_name']
+            user.first_name = request.POST.get('first_name')
+            user.last_name = request.POST.get('last_name')
             user.save()
 
-            if user_form.is_valid() and certification_formset.is_valid() and language_formset.is_valid():
-                user_form.save()
+            # Save user profile
+            user_profile = user_form.save(commit=False)
+            user_profile.user = user
+            user_profile.save()
 
-                # Deleting marked certifications
-                for form in certification_formset.deleted_forms:
-                    if form.cleaned_data.get('id'):
-                        form.instance.delete()
+            # Handle profile image
+            if 'profile_image' in request.FILES:
+                user_profile.profile_image = request.FILES['profile_image']
+                user_profile.save()
 
-                # Deleting marked languages
-                for form in language_formset.deleted_forms:
-                    if form.cleaned_data.get('id'):
-                        form.instance.delete()
+            # Save certifications and languages
+            certification_formset.instance = user_profile
+            certification_formset.save()
+            language_formset.instance = user_profile
+            language_formset.save()
 
-                # Saving valid certifications
-                for form in certification_formset:
-                    if form.is_valid() and not form.cleaned_data.get('id'):
-                        certification = form.save(commit=False)
-                        if certification.title and certification.issuing_organization:
-                            certification.user_profile = user_profile
-                            certification.save()
+            messages.success(request, "Profile updated successfully.")
+            return redirect('IntroHome')
+        else:
+            messages.error(request, "Please correct the errors below.")
 
-                # Saving valid languages
-                for form in language_formset:
-                    if form.is_valid() and not form.cleaned_data.get('id'):
-                        language = form.save(commit=False)
-                        if language.language and language.proficiency:
-                            language.user_profile = user_profile
-                            language.save()
-
-                # Redirecting to the user's introductory home page
-                return redirect('IntroHome')
-
-    # Updating context with form instances and other necessary data
-    context.update({
+    context = {
         'user_form': user_form,
         'certification_formset': certification_formset,
         'language_formset': language_formset,
-        'username_taken': username_taken,
-        'email_taken': email_taken,
-    })
-    return render(request, 'edit_profile.html', context)
+        'user': user,
+        'user_profile': user_profile,
+    }
 
+    return render(request, 'edit_profile.html', context)
 # View function for viewing public profile of a user
 @login_required()  # Requiring login for access
 def view_profile_public(request, username):
